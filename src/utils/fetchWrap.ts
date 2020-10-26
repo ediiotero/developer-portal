6d4297164ac61625e5e1165d9560942dddbcfb07
TS000 <tylersnell@me.com>
2020-10-26 14:43:34 -0600

    Handle error ID at the source

--- src/utils/fetchWrap.ts
+++ src/utils/fetchWrap.ts
@@ -1,46 +1,22 @@
 import * as Sentry from '@sentry/browser';
-import { v4 as uuidv4 } from 'uuid';
 
-
-// ===== Start page specific data ===== 
-const newID: string = uuidv4();
-const newHeaders = new Headers();
-newHeaders.append('reqID', newID);
-
-const newInit: RequestInit = {
-  cache: 'default',
-  headers: newHeaders,
-  method: 'GET',
-  mode: 'cors',
-};
-
-const debugURL = 'http://localhost:3030/url';
-
-export const fetchRequest = new Request(debugURL, newInit);
-
-// ===== End page specific data =====
-
-const errorFunc = (error: string) => {
+const errorFunc = (error: string, errorID: string) => {
   Sentry.withScope(scope => {
-    scope.setTag('Error ID', newID);
+    scope.setTag('Error ID', errorID);
     Sentry.captureException(error);
   });
 };
-
-export const fetchWrap = async (request: Request) => {
-  console.time('timer starts');
+ 
+export const fetchWrap = async (request: Request, errorID: string) => {
   try {
     const response = await fetch(request);
     if ((!response.ok && response.status !== 300) || 400 || 500) {
-      throw TypeError(`Front End Error ID: ${newID}`);
+      throw TypeError(`Front End Error ID: ${errorID}`);
     }
     return response;
     
   } catch (error) {
-    return errorFunc(error);
-  } finally {
-    // not sure what to do here. Timer for science.
-    console.timeEnd('timer ends');
+    return errorFunc(error, errorID);
   }
 };
 
