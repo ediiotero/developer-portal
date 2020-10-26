a23b851eb520d931d7a783b8ecd8d0ab1f68ca6e
TS000 <tylersnell@me.com>
2020-10-26 15:14:59 -0600

    cleaned it up a bit

--- src/utils/fetchWrap.ts
+++ src/utils/fetchWrap.ts
@@ -1,22 +1,25 @@
 import * as Sentry from '@sentry/browser';
 
-const errorFunc = (error: string, errorID: string) => {
+const sentryErrorHandler = (error: string, errorID: string) => {
   Sentry.withScope(scope => {
     scope.setTag('Error ID', errorID);
     Sentry.captureException(error);
   });
+  return error;
 };
- 
+
+const fetchHandler = (response: Response, errorID: string) => {
+  if ((!response.ok && response.status !== 300) || 400 || 500) {
+    throw TypeError(`Front End Error ID: ${errorID}`);
+  }
+  return response;
+};
+
 export const fetchWrap = async (request: Request, errorID: string) => {
   try {
     const response = await fetch(request);
-    if ((!response.ok && response.status !== 300) || 400 || 500) {
-      throw TypeError(`Front End Error ID: ${errorID}`);
-    }
-    return response;
-    
+    fetchHandler(response, errorID);
   } catch (error) {
-    return errorFunc(error, errorID);
+    return sentryErrorHandler(error, errorID);
   }
 };
-
